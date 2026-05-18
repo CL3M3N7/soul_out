82d541a17c78f29cb22b3d433400ce4c7054795c
CL3M3N7 <146713907+CL3M3N7@users.noreply.github.com>
2026-05-18 16:45:17 -0400

    feat(character): add animation

--- soul_out/Scripts/Characters/Character.cs
+++ soul_out/Scripts/Characters/Character.cs
@@ -8,35 +8,48 @@ public partial class Character : CharacterBody2D
 	
 	public const float Speed = 300.0f;
 	public const float JumpVelocity = -400.0f;
+	private AnimatedSprite2D _animatedSprite2D = null;
 
 	public override void _Ready()
 	{
+		_animatedSprite2D = GetNode<CharacterSprite>("Sprite");
 	}
 
 	public override void _PhysicsProcess(double delta)
 	{
 		Vector2 velocity = Velocity;
-
-		// Handle Jump.
-		/*if (Input.IsActionJustPressed("ui_accept") && IsOnFloor())
-	{
-		velocity.Y = JumpVelocity;
-	}*/
 		
 		Vector2 direction = Input.GetVector(
 			$"SOMoveLeft_{PlayerController}", $"SOMoveRight_{PlayerController}",
 			$"SOMoveUp_{PlayerController}", $"SOMoveDown_{PlayerController}"
 		);
+		
 		if (direction != Vector2.Zero)
 		{
 			velocity = direction * Speed;
 		}
 		else
 		{
-			velocity.X = Mathf.MoveToward(Velocity.X, 0, Speed);
-			velocity.Y = Mathf.MoveToward(Velocity.Y, 0, Speed);
+			velocity.X = 0;
+			velocity.Y = 0;
 		}
+		
+		if (_animatedSprite2D != null)
+		{
+			_animatedSprite2D.FlipH = velocity.X switch
+			{
+				> 0 => false,
+				< 0 => true,
+				_ => _animatedSprite2D.FlipH
+			};
 
+			_animatedSprite2D.Play(velocity != Vector2.Zero ? "run" : "idle");
+		}
+		else
+		{
+			GD.PrintErr("[Character] No animated sprite attached!");
+		}
+		
 		Velocity = velocity;
 		MoveAndSlide();
 	}
