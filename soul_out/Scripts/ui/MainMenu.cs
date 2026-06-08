14f05b83427446b32b9e5249fe7a93a4a492ee48
DyablOcOrps <bap.dubois24@gmail.com>
2026-06-08 14:11:53 -0400

    [UPDATE] Options Menu Audio

--- soul_out/Scripts/ui/MainMenu.cs
+++ soul_out/Scripts/ui/MainMenu.cs
@@ -4,37 +4,72 @@ using SoulOut.Scripts.Manager;
 
 public partial class MainMenu : Control
 {
+	private HSlider _masterSlider;
+	private HSlider _musicSlider;
+	private HSlider _sfxSlider;
+
+	// On stocke les index de nos bus audio pour aller plus vite
+	private int _masterBusIndex;
+	private int _musicBusIndex;
+	private int _sfxBusIndex;
+
 	private Button _startButton;
 	private Button _optionsButton;
 	private Button _quitButton;
 	
+	private Button _quitOptionsButton;
+	
 	// Optionnel : un panel qui contient tes options (à cacher/afficher)
-	private Control _optionsMenu; 
+	private Panel _optionsMenu; 
+	private VBoxContainer _mainMenuButtons;
 	[Export] private Control _mainMenuPanel;
 	[Export] private Control _playerSelectionPanel;
 
 	public override void _Ready()
 	{
-		// 2. On récupère les nœuds de la scène
-		// ATTENTION : Remplace "VBoxContainer/StartButton" par le vrai chemin de tes boutons dans ta scène Godot !
-		_startButton = GetNode<Button>("VBoxContainer/StartButton");
-		_optionsButton = GetNode<Button>("VBoxContainer/OptionsButton");
-		_quitButton = GetNode<Button>("VBoxContainer/QuitButton");
-		
-		
-		// _optionsMenu = GetNode<Control>("OptionsMenuPanel");
-		// _optionsMenu.Hide(); // On s'assure qu'il est caché au lancement
+		// 1. Récupération des index des bus
+		_masterBusIndex = AudioServer.GetBusIndex("Master");
+		_musicBusIndex = AudioServer.GetBusIndex("Music");
+		_sfxBusIndex = AudioServer.GetBusIndex("SFX");
 
-		// 3. LA MAGIE DU C# : On connecte les signaux de clic aux fonctions
+		// 2. Récupération de tes nœuds Sliders 
+		_masterSlider = GetNode<HSlider>("OptionsPanel/MusicVBoxContainer/GlobalMusicContainer/Slider");
+		_musicSlider = GetNode<HSlider>("OptionsPanel/MusicVBoxContainer/SfxMusicContainer/Slider");
+		_sfxSlider = GetNode<HSlider>("OptionsPanel/MusicVBoxContainer/MusicContainer/Slider");
+
+		// 3. On initialise la position des sliders par rapport au volume actuel du jeu
+		_masterSlider.Value = Mathf.DbToLinear(AudioServer.GetBusVolumeDb(_masterBusIndex));
+		_musicSlider.Value = Mathf.DbToLinear(AudioServer.GetBusVolumeDb(_musicBusIndex));
+		_sfxSlider.Value = Mathf.DbToLinear(AudioServer.GetBusVolumeDb(_sfxBusIndex));
+
+		// 4. On connecte le signal de changement de valeur
+		_masterSlider.ValueChanged += OnMasterSliderValueChanged;
+		_musicSlider.ValueChanged += OnMusicSliderValueChanged;
+		_sfxSlider.ValueChanged += OnSfxSliderValueChanged;
+		
+		_startButton = GetNode<Button>("ButtonMenuContainer/StartButton");
+		_optionsButton = GetNode<Button>("ButtonMenuContainer/OptionsButton");
+		_quitButton = GetNode<Button>("ButtonMenuContainer/QuitButton");
+		
+		_quitOptionsButton = GetNode<Button>("OptionsPanel/QuitOptionsButton");
+		
+		_mainMenuButtons = GetNode<VBoxContainer>("ButtonMenuContainer");
+		_mainMenuButtons.Show();
+		_optionsMenu = GetNode<Panel>("OptionsPanel");
+		_optionsMenu.Hide(); // On s'assure qu'il est caché au lancement
+
 		_startButton.Pressed += OnStartButtonPressed;
 		_optionsButton.Pressed += OnOptionsButtonPressed;
 		_quitButton.Pressed += OnQuitButtonPressed;
+		
+		_quitOptionsButton.Pressed += OnQuitOptionsButtonPressed;
 		
 		if(_startButton != null)
 		{
 			_startButton.GrabFocus();
 		}
 	}
+	
 	
 	private void OnStartButtonPressed()
 	{
@@ -51,10 +86,9 @@ public partial class MainMenu : Control
 	{
 		GD.Print("Menu des options ouvert !");
 		
-		// Si ton menu d'options est dans la même scène, tu l'affiches simplement :
-		// _optionsMenu.Show();
-		
-		// (Tu pourras mettre un bouton "Retour" dans tes options qui fera _optionsMenu.Hide(); )
+		_optionsMenu.Show();
+		_mainMenuButtons.Hide();
+		_quitOptionsButton.GrabFocus();
 	}
 
 	private void OnQuitButtonPressed()
@@ -63,5 +97,31 @@ public partial class MainMenu : Control
 		
 		// La fonction officielle de Godot pour fermer l'application proprement
 		GetTree().Quit();
+	}
+	
+	private void OnQuitOptionsButtonPressed()
+	{
+		_optionsMenu.Hide();
+		_mainMenuButtons.Show();
+		_startButton.GrabFocus();
+	}
+	
+	private void OnMasterSliderValueChanged(double value)
+	{
+		// On convertit la valeur du slider (0 à 1) en Décibels, puis on l'applique au bus
+		float dbValue = (float)Mathf.LinearToDb(value);
+		AudioServer.SetBusVolumeDb(_masterBusIndex, dbValue);
+	}
+
+	private void OnMusicSliderValueChanged(double value)
+	{
+		float dbValue = (float)Mathf.LinearToDb(value);
+		AudioServer.SetBusVolumeDb(_musicBusIndex, dbValue);
+	}
+
+	private void OnSfxSliderValueChanged(double value)
+	{
+		float dbValue = (float)Mathf.LinearToDb(value);
+		AudioServer.SetBusVolumeDb(_sfxBusIndex, dbValue);
 	}
 }
